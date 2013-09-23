5a975a77110d8b2458a87eb7efed108c874f3d7a
John Sullivan <johnsully83@gmail.com>
2013-09-22 22:53:48 -0400

    Removed target package

--- target/johnsully83/WEB-INF/classes/javascript/prime/cleaning/map.js
+++
@@ -1,9 +0,0 @@
-function() {
-	if(lastPrimeID >= 0) {
-		if(lastPrimeID != (this.id - 1)) {
-			emit(lastPrimeID+1, {nextKnownPrime: this.prime});
-		}
-		
-		lastPrimeID = this.id;
-	}
-}
--- target/johnsully83/WEB-INF/jsp/tags/header.jsp
+++
@@ -1,1 +0,0 @@
-<div id="headerWrapper"><h1 id="siteHeader" class="header"><a href="/" id="headerLink">johnSully83.com</a></h1></div>
